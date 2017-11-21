1dc603ea8c432bd38120cdeb48b7a478bea43e82
Emaronti <emaronti@hotmail.it>
2017-11-21 20:53:11 +0100

    Undicesimo commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+<<<<<<< HEAD
+//Questo commento serve per creare il conflitto
+=======
+//Questo commento serve per creare il conflittooooo
+//Provaprova
+>>>>>>> FIX_BUG
+void main(){
+	for(int i=0;i<10;i++)
+	{
+		cout<<i;
+	}
+}
